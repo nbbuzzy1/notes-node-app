c0e65cd9c9aa31586a15b8b79e4729ac2a4f9bb0
Nick Buzzy <nick.buzzy@chmuraecon.com>
2019-12-03 16:05:09 -0500

    Added yargs; implemented initial add, remove, read, list commands.

--- app.js
+++ app.js
@@ -1,8 +1,65 @@
 const chalk = require('chalk');
- 
+const yargs = require('yargs');
+
 const getNotes = require('./notes.js');
 
-const notesMessage = getNotes();
+// Customize yargs version
+yargs.version('1.1.0');
 
-console.log(chalk.red.inverse('Error!'));
-console.log(notesMessage);
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Notes title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Notes body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        console.log('Title: ', argv.title);
+        console.log('Body: ', argv.body);
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Notes title'
+        }
+    },
+    handler(argv) {
+        console.log('Removing the note');
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler() {
+        console.log('Reading the note');
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'Listing the notes',
+    handler() {
+        console.log('Listing the notes');
+    }
+})
+
+// add, remove, read, list
+
+yargs.parse();

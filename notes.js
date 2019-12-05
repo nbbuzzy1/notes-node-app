9767ab3abed648d5bc1c3292e89643d4ed8dd47a
Nick Buzzy <nick.buzzy@chmuraecon.com>
2019-12-05 13:14:18 -0500

    Created addNote, saveNotes, and loadNotes.

--- notes.js
+++ notes.js
@@ -1,3 +1,46 @@
+const fs = require('fs');
+
 const getNotes = () => 'Your notes...';
 
-module.exports = getNotes;
+const addNote = (title, body) => {
+    const notes = loadNotes();
+    const duplicateNotes = notes.filter((note) => note.title === title);
+    console.log(duplicateNotes);
+
+    if (!duplicateNotes.length) {
+        notes.push({
+            title,
+            body
+        })
+        saveNotes(notes);
+        console.log(notes);
+    } else {
+        console.log('Note title taken', notes);
+    }
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json', dataJSON);
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json');
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    }
+}
+
+const removeNotes = (removedNoteTitle) => {
+    const notes = loadNotes();
+    const newNotes = notes.filter((note) => note.title !== removedNoteTitle);
+    saveNotes(newNotes);
+}
+
+module.exports = {
+    getNotes, 
+    addNote
+};
